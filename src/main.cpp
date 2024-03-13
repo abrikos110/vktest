c2bc1068cf15f75a6caac28c1260c03a60f09f24
abrikos110 <abrikos1100@gmail.com>
2024-03-13 16:33:04 +0300

    started doing arch for raytracing

--- src/main.cpp
+++ src/main.cpp
@@ -20,8 +20,28 @@ bool in_triangle(flt2 x, flt2 a, flt2 b, flt2 c) {
 struct scene {
     unsigned w, h;
     std::vector<unsigned> image;
+    std::vector<flt4> rays;
 
-    scene(unsigned w, unsigned h) : w(w), h(h), image(w * h) {}
+    scene(unsigned w, unsigned h, float vfov = 90)
+        : w(w), h(h), image(w * h), rays(w * h) {
+        // camera at (0,0,0) watching to (0,0,1)
+        // x direction is (1,0,0), y direction is (0,1,0)
+        // image coords are from -1 to 1 for y and from -w/h to w/h for x
+        flt4 dir{0, 0, 1};
+        vfov *= M_PI / 180;
+        for (unsigned i = 0; i < w; ++i) {
+            for (unsigned j = 0; j < h; ++i) {
+                flt4 r(i2x(i), j2y(j), 0);
+                float n = std::sqrt(dot(r, r));
+                r /= (n ? n : 1);
+                float angle = n * vfov / 2;
+                rays[i*h + j] = std::cos(angle) * dir + std::sin(angle) * r;
+            }
+        } // TODO stupid_tracing
+    }
+
+    float i2x(unsigned i) { return (2.f * i - w) / h; }
+    float j2y(unsigned j) { return (2.f * j - h) / h; }
 
     flt4 calc_pixel(int i, int j) {
         flt4 ans{(i + 1.f) / w, (1.f + j) / h, 0.5};
